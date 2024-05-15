34ac7dd0d046b8d94e8dd40eb0d0445b8aeb7452
utkarsh maurya <itsutkarsh123@gmail.com>
2024-05-15 11:29:47 +0530

    feat: Add Experience component to display work experience

---
+++ src/app/components/Experience.tsx
@@ -0,0 +1,29 @@
+const Experience = () => {
+	return (
+		<div className="min-h-screen flex flex-col justify-center sm:justify-normal sm:mt-20">
+			<div className="">
+				<div className="mb-16">
+					<h2 className="text-6xl font-bold  sm:text-4xl">my work experience 🧑🏻‍💻🗃️</h2>
+				</div>
+				<div>
+					<div>
+						<div className="flex justify-between">
+							<h3 className="text-3xl font-semibold">Frontend Intern</h3>
+							<p className="font-semibold">March 2024 - Present</p>
+						</div>
+						<p>Sumatak Technologies</p>
+						<ul className=" list-disc list-inside mt-3 text-gray-600 dark:text-dark-secondary">
+              <li>
+                Worked on the company&apos;s mobile app
+              </li>
+							<li>Used React.js, Tailwind CSS, and TypeScript</li>
+							<li>Worked on the company&apos;s mobile app</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Experience;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import AboutMe from "./components/AboutMe";
+import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 export default function Home() {
 	return (
-		<div className="min-h-screen flex flex-col items-center overflow-y-scroll scrollbar-hide snap-y snap-mandatory h-screen bg-primary dark:bg-dark-primary text-primary dark:text-dark-primary">
+		<div className="min-h-screen flex flex-col items-center overflow-y-scroll scrollbar-hide snap-y snap-mandatory h-screen bg-primary dark:bg-dark-primary text-primary dark:text-dark-primary sm:snap-none">
 			<div className="w-3/4 snap-center sm:w-5/6">
 				<Hero />
 			</div>
 			<div className="w-3/4 snap-center sm:w-5/6">
 				<AboutMe />
+			</div>
+			<div className="w-3/4 snap-center sm:w-5/6 ">
+				<Experience />
 			</div>
 			<div className="w-3/4 snap-center sm:w-5/6 ">
 				<Skills />
