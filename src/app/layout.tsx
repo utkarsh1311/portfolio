28ff5df067fd44ec652b5d0ea22fd96851e0dec1
utkarsh maurya <itsutkarsh123@gmail.com>
2024-07-11 18:56:26 +0530

    style: Update metadata for SEO optimization and improved user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,32 @@ const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Utkarsh Maurya",
-	description: "Home page for Utkarsh Maurya portfolio website",
+	description: `I'm Utkarsh Maurya, a software engineer who loves to build things for the web. I specialize in building high-quality websites and applications with a focus on performance and user experience.`,
+	creator: "Utkarsh Maurya",
+	openGraph: {
+    title: 'Utkarsh Maurya',
+    description: 'I\'m Utkarsh Maurya, a software engineer who loves to build things for the web. I specialize in building high-quality websites and applications with a focus on performance and user experience.',
+    url: 'https://utkarshmaurya.me',
+    siteName: 'Utkarsh\'s Portfolio',
+    images: [
+      {
+        url: 'https://ik.imagekit.io/utkarsh1311/portfolio.png?updatedAt=1720704145574&tr=w-1200%2Ch-675%2Cfo-auto', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'Utkarsh Maurya portfolio website image',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+	},
+	twitter: {
+    title: 'Utkarsh Maurya',
+    description: 'I\'m Utkarsh Maurya, a software engineer who loves to build things for the web. I specialize in building high-quality websites and applications with a focus on performance and user experience.',
+    creator: '@coder_utkarsh',
+    images: ['https://ik.imagekit.io/utkarsh1311/portfolio.png?updatedAt=1720704145574&tr=w-1200%2Ch-675%2Cfo-auto'], // Must be an absolute URL
+	},
+	category: "Portfolio",
+	
 };
 
 export default function RootLayout({
@@ -27,51 +52,6 @@ export default function RootLayout({
 				<meta
 					name="google-site-verification"
 					content="T1wRFIfvEePSaYspISPTOf8EeR05JF2XjtRT7ilL-DQ"
-				/>
-				{/* <!-- Primary Meta Tags --> */}
-				<title>Utkarsh Maurya </title>
-				<meta name="title" content="Utkarsh Maurya " />
-				<meta
-					name="description"
-					content="I'm a full-stack developer who thrives on crafting seamless websites. Coding isn't just a job—it's my creative outlet to bring ideas to life, from frontend to backend.
-
-Beyond the screen, I find joy in animated worlds, indulging my passion for animes during leisure hours.
-
-In my downtime, you'll catch me immersed in a good book. Reading is my cherished pastime, allowing me to unwind, broaden my perspectives, and explore worlds beyond the lines of code."
-				/>
-
-				{/* <!-- Open Graph / Facebook --> */}
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://metatags.io/" />
-				<meta property="og:title" content="Utkarsh Maurya " />
-				<meta
-					property="og:description"
-					content="I'm a full-stack developer who thrives on crafting seamless websites. Coding isn't just a job—it's my creative outlet to bring ideas to life, from frontend to backend.
-
-Beyond the screen, I find joy in animated worlds, indulging my passion for animes during leisure hours.
-
-In my downtime, you'll catch me immersed in a good book. Reading is my cherished pastime, allowing me to unwind, broaden my perspectives, and explore worlds beyond the lines of code."
-				/>
-				<meta
-					property="og:image"
-					content="https://drive.google.com/file/d/1p6We5YKoasCQqeJiZjFZAAF9DY_tBbkN/view?usp=sharing"
-				/>
-
-				{/* <!-- Twitter --> */}
-				<meta property="twitter:card" content="summary_large_image" />
-				<meta property="twitter:url" content="https://metatags.io/" />
-				<meta property="twitter:title" content="Utkarsh Maurya " />
-				<meta
-					property="twitter:description"
-					content="I'm a full-stack developer who thrives on crafting seamless websites. Coding isn't just a job—it's my creative outlet to bring ideas to life, from frontend to backend.
-
-Beyond the screen, I find joy in animated worlds, indulging my passion for animes during leisure hours.
-
-In my downtime, you'll catch me immersed in a good book. Reading is my cherished pastime, allowing me to unwind, broaden my perspectives, and explore worlds beyond the lines of code."
-				/>
-				<meta
-					property="twitter:image"
-					content="https://drive.google.com/file/d/1p6We5YKoasCQqeJiZjFZAAF9DY_tBbkN/view?usp=sharing"
 				/>
 			</head>
 			<body className={`${poppins.className} bg-primary dark:bg-dark-primary`}>
