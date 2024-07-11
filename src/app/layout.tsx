df47d70f017da7f2eae0fbf6d23314fcf745a028
utkarsh maurya <itsutkarsh123@gmail.com>
2024-07-11 18:13:14 +0530

    style: Update meta tags for SEO optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,55 @@ export default function RootLayout({
 		<html lang="en" suppressHydrationWarning>
 			<head>
 				<link rel="shortcut icon" href="/favicon.ico" type="image/gif" />
-				<meta name="google-site-verification" content="T1wRFIfvEePSaYspISPTOf8EeR05JF2XjtRT7ilL-DQ" />
+				<meta
+					name="google-site-verification"
+					content="T1wRFIfvEePSaYspISPTOf8EeR05JF2XjtRT7ilL-DQ"
+				/>
+				{/* <!-- Primary Meta Tags --> */}
+				<title>Utkarsh Maurya </title>
+				<meta name="title" content="Utkarsh Maurya " />
+				<meta
+					name="description"
+					content="I'm a full-stack developer who thrives on crafting seamless websites. Coding isn't just a job—it's my creative outlet to bring ideas to life, from frontend to backend.
+
+Beyond the screen, I find joy in animated worlds, indulging my passion for animes during leisure hours.
+
+In my downtime, you'll catch me immersed in a good book. Reading is my cherished pastime, allowing me to unwind, broaden my perspectives, and explore worlds beyond the lines of code."
+				/>
+
+				{/* <!-- Open Graph / Facebook --> */}
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://metatags.io/" />
+				<meta property="og:title" content="Utkarsh Maurya " />
+				<meta
+					property="og:description"
+					content="I'm a full-stack developer who thrives on crafting seamless websites. Coding isn't just a job—it's my creative outlet to bring ideas to life, from frontend to backend.
+
+Beyond the screen, I find joy in animated worlds, indulging my passion for animes during leisure hours.
+
+In my downtime, you'll catch me immersed in a good book. Reading is my cherished pastime, allowing me to unwind, broaden my perspectives, and explore worlds beyond the lines of code."
+				/>
+				<meta
+					property="og:image"
+					content="https://drive.google.com/file/d/1p6We5YKoasCQqeJiZjFZAAF9DY_tBbkN/view?usp=sharing"
+				/>
+
+				{/* <!-- Twitter --> */}
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://metatags.io/" />
+				<meta property="twitter:title" content="Utkarsh Maurya " />
+				<meta
+					property="twitter:description"
+					content="I'm a full-stack developer who thrives on crafting seamless websites. Coding isn't just a job—it's my creative outlet to bring ideas to life, from frontend to backend.
+
+Beyond the screen, I find joy in animated worlds, indulging my passion for animes during leisure hours.
+
+In my downtime, you'll catch me immersed in a good book. Reading is my cherished pastime, allowing me to unwind, broaden my perspectives, and explore worlds beyond the lines of code."
+				/>
+				<meta
+					property="twitter:image"
+					content="https://drive.google.com/file/d/1p6We5YKoasCQqeJiZjFZAAF9DY_tBbkN/view?usp=sharing"
+				/>
 			</head>
 			<body className={`${poppins.className} bg-primary dark:bg-dark-primary`}>
 				<Providers>
